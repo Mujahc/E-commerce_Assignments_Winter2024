f62a8bf63bc5867bda313625fd49efe672c0505b
Johann Culla-ag 2147185 <89601928+Mujahc@users.noreply.github.com>
2024-02-23 23:55:29 -0500

    Did some designs

--- app/views/Contact/index.php
+++ app/views/Contact/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
     <title><?= $name ?> view</title>
     <link rel="stylesheet" href="/resources/contact.css">
+    <link rel="stylesheet" href="/resources/general.css">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
@@ -23,15 +24,17 @@
     <p>Wanna reach us? Write your email information and message in the following form and then submit.</p>
 
     <form action="/Contact/submit">
-        <div>
+        <div class="form-group">
             <label for="email">Email:</label>
             <input type="email" id="email">
         </div>
-        <div>
+        <div class="form-group">
             <label for="message">Message:</label>
             <textarea name="message" id="message" cols="30" rows="10"></textarea>
         </div>
-        <button id="send_Btn">Send!</button>
+        <div class="form-group">
+            <button id="send_Btn">Send!</button>
+        </div>
     </form>
 
     <p id="numOfPageVisits">0 page visits.</p>
--- app/views/Main/about_us.php
+++ app/views/Main/about_us.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
     <title><?= $name ?> view</title>
     <link rel="stylesheet" href="/resources/main.css">
+    <link rel="stylesheet" href="/resources/general.css">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
@@ -21,16 +22,18 @@
 
     <h1>About Us</h1>
     <p>See our incredible team of devs</p>
-    <div>
-        <div>
+    <div id="devs-pictures">
+        <div class="picture-group">
             <img src="/resources/male_devs_image.jpg" alt="Loading...">
             <p>Bob loves making money on the stock market floor.</p>
         </div>
-        <div>
+        <div class="picture-group">
             <img src="/resources/female_devs_image.jpg" alt="Loading...">
             <p>Amy keeps Johnny in check</p>
         </div>
     </div>
+
+    <p id="numOfPageVisits">0 page visits.</p>
     
 </body>
 </html>
